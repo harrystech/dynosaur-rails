88dee322441a2dd9452350cb185484e3450c5672
Andy O'Neill <andy@potatoriot.com>
2013-11-12 15:29:44 -0500

    Robustify initializer (swallow exception)

--- config/initializers/dynosaur.rb
+++ config/initializers/dynosaur.rb
@@ -3,9 +3,16 @@ puts "Running dynosaur initializers"
 
 require "dynosaur"
 
-config = ScalerConfig.last(:order => "id asc", :limit => 1)
 
-if !config.nil?
-  Dynosaur.initialize(config.get_hash)
-  Dynosaur.start_in_thread
+begin
+  config = ScalerConfig.last(:order => "id asc", :limit => 1)
+
+  if !config.nil?
+    Dynosaur.initialize(config.get_hash)
+    Dynosaur.start_in_thread
+  end
+rescue Exception => e
+  puts e
+  puts "Oh noes! Couldn't initialize Dynosaur (is the db ready yet?)"
+  puts "WARNING: NOT RUNNING DYNOSAUR!"
 end
