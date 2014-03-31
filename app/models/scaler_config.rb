35d3f73fadf7f80b6312f92cd959847ced955f13
Andy O'Neill <aoneill@harrys.com>
2014-03-31 17:03:21 -0400

    Change 'blackout' to 'hysteresis_period'
    - Update to new version of dynosaur
    - Use new plugin parameters

--- app/models/scaler_config.rb
+++ app/models/scaler_config.rb
@@ -1,8 +1,8 @@
 class ScalerConfig < ActiveRecord::Base
   has_many :plugin_configs
-  attr_accessible :blackout, :dry_run, :heroku_api_key, :heroku_app_name, :interval, :max_web_dynos, :min_web_dynos, :librato_email, :librato_api_key
+  attr_accessible :dry_run, :heroku_api_key, :heroku_app_name, :interval, :max_web_dynos, :min_web_dynos, :librato_email, :librato_api_key
 
-  validates :heroku_app_name, :heroku_api_key, :interval, :max_web_dynos, :min_web_dynos, :blackout, :presence => true
+  validates :heroku_app_name, :heroku_api_key, :interval, :max_web_dynos, :min_web_dynos, :presence => true
 
   def get_hash
     h = {
@@ -15,7 +15,6 @@ class ScalerConfig < ActiveRecord::Base
         "librato_email" => self.librato_email,
         "librato_api_key" => self.librato_api_key,
         "interval" => self.interval,
-        "blackout" => self.blackout,
       }
     }
     plugins = []
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131231212515) do
+ActiveRecord::Schema.define(:version => 20140328155438) do
 
   create_table "plugin_config_items", :force => true do |t|
     t.integer  "plugin_config_id"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(:version => 20131231212515) do
     t.string   "name"
     t.string   "plugin_type"
     t.integer  "interval"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.integer  "hysteresis_period"
   end
 
   create_table "scaler_configs", :force => true do |t|
